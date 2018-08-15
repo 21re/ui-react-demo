08cbd7bf667a6f0868feeda9e6bb7db8b488e7ed
Max Kiehnscherf <max.kiehnscherf@gmail.com>
2018-08-15 13:37:58 +0200

    [fix] es6-promise as standard dep
    es6-promise seems to include a polyfill for IE11, which we need for
    certain customers

--- ui/app.tsx
+++ ui/app.tsx
@@ -3,6 +3,9 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { MainFrame } from "./main-frame";
+import * as promise from "es6-promise";
+
+promise.polyfill();
 
 render((
   <Provider store={store}>
